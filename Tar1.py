0c73314367fa38450641a84b99a6411900e0dee7
Bengalson <80784350+Bengalson@users.noreply.github.com>
2024-02-29 18:48:06 +0200

    Add files via upload

---
+++ Tar1.py
@@ -0,0 +1,18 @@
+list = [1,6,4,8,34,7]
+
+largest = list[0]
+
+for i in list:
+    if i>largest:
+        largest = i
+
+print(F"The largest number is: {largest}")
+
+   
+              
+'''      
+ list = [1,6,8,2,4,5]
+
+list.sort()    או  list.mam            <- זה בדרך קצרה יותר
+
+print(list[-1])    או    print(list)   '''
